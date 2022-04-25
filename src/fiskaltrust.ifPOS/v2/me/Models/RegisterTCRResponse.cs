6b5e01ddbf550387677168c1552a546124171f2a
Tom Schmiedlechner <tschmiedlechner@outlook.com>
2022-04-25 12:59:19 +0200

    Added responses

--- src/fiskaltrust.ifPOS/v2/me/Models/RegisterTCRResponse.cs
+++ src/fiskaltrust.ifPOS/v2/me/Models/RegisterTCRResponse.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
+﻿using System.Runtime.Serialization;
 
 namespace fiskaltrust.ifPOS.v2.me
 {
     [DataContract]
     public class RegisterTcrResponse
     {
+        /// <summary>
+        /// Unique code of the cash register, assigned by the central invoice register (CIS) service. Must be used in all subsequent requests to the CIS service.
+        /// </summary>
+        /// <remarks>
+        /// Has the following format: [a-z]{2}[0-9]{3}[a-Z]{2}[0-9]{3} (e.g. ab123ab123)
+        /// </remarks>
+        [DataMember(Order = 10)]
+        public string TcrCode { get; set; }
     }
 }
