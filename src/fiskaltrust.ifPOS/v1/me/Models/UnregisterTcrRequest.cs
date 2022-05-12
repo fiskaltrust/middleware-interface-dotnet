0b5ca5c9817800821f185a3e7f063a56a86027e9
Paul Volavsek <paul.volavsek@gmail.com>
2022-05-12 13:55:49 +0200

    fix unregister tcr request

--- src/fiskaltrust.ifPOS/v1/me/Models/UnregisterTcrRequest.cs
+++ src/fiskaltrust.ifPOS/v1/me/Models/UnregisterTcrRequest.cs
@@ -4,24 +4,5 @@ using System.Runtime.Serialization;
 namespace fiskaltrust.ifPOS.v1.me
 {
     [DataContract]
-    public class UnregisterTcrRequest
-    {
-        /// <summary>
-        /// Message identifier, equal to ftQueueItemId when used by fiskaltrust.
-        /// </summary>
-        [DataMember(Order = 10)]
-        public Guid RequestId { get; set; }
-
-        /// <summary>
-        /// Code of the business unit in which the invoice is issued.
-        /// </summary>
-        [DataMember(Order = 20)]
-        public string BusinessUnitCode { get; set; }
-
-        /// <summary>
-        /// Element representing the internal identification of the TCR.
-        /// </summary>
-        [DataMember(Order = 30)]
-        public string InternalTcrIdentifier { get; set; }
-    }
+    public class UnregisterTcrRequest : RegisterTcrRequest { }
 }
