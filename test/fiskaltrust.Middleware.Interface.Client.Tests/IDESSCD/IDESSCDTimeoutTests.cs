cbdcc26a884c57eb5ea816c01d60e7ee696f4bb9
Paul Volavsek <paul.volavsek@gmail.com>
2022-06-09 11:11:30 +0200

    fix tests port

--- test/fiskaltrust.Middleware.Interface.Client.Tests/IDESSCD/IDESSCDTimeoutTests.cs
+++ test/fiskaltrust.Middleware.Interface.Client.Tests/IDESSCD/IDESSCDTimeoutTests.cs
@@ -4,6 +4,8 @@ using NUnit.Framework;
 using System.Threading.Tasks;
 using fiskaltrust.ifPOS.v1.de;
 using System.Net;
+using System.Linq;
+using System.Net.NetworkInformation;
 
 namespace fiskaltrust.Middleware.Interface.Client.Tests.IDESSCD
 {
@@ -18,13 +20,13 @@ namespace fiskaltrust.Middleware.Interface.Client.Tests.IDESSCD
         [Test]
         public async Task HttpShouldTimeout()
         {
-            HttpListener listener = new HttpListener();
-            listener.Prefixes.Add("http://localhost:5501/");
+            using HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:5502/");
 
             var client = await Client.Http.HttpDESSCDFactory.CreateSSCDAsync(new Client.Grpc.GrpcClientOptions
             {
                 RetryPolicyOptions = _retryPolicyOptions,
-                Url = new Uri("http://localhost:5501")
+                Url = new Uri("http://localhost:5502")
             });
 
             listener.Start();
@@ -38,7 +40,7 @@ namespace fiskaltrust.Middleware.Interface.Client.Tests.IDESSCD
         [Test]
         public async Task GrpcShouldTimeout()
         {
-            HttpListener listener = new HttpListener();
+            using HttpListener listener = new HttpListener();
             listener.Prefixes.Add("http://localhost:5501/");
 
             var client = await Client.Grpc.GrpcDESSCDFactory.CreateSSCDAsync(new Client.Grpc.GrpcClientOptions
