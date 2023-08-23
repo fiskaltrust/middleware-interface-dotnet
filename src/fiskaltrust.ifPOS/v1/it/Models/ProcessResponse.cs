bc1d3311a578cd7b830ebd9abbdfa2e1098a785f
Stefan Kert <stefan.kert@fiskaltrust.at>
2023-08-23 18:44:12 +0200

    Update ProcessResponse.cs

--- src/fiskaltrust.ifPOS/v1/it/Models/ProcessResponse.cs
+++ src/fiskaltrust.ifPOS/v1/it/Models/ProcessResponse.cs
@@ -6,13 +6,6 @@ namespace fiskaltrust.ifPOS.v1.it
     public class ProcessResponse
     {
         [DataMember(Order = 10)]
-        public ReceiptRequest ReceiptRequest { get; set; }
-
-        [DataMember(Order = 20)]
         public ReceiptResponse ReceiptResponse { get; set; }
-
-
-        [DataMember(Order = 30)]
-        public bool Print { get; set; }
     }
 }
