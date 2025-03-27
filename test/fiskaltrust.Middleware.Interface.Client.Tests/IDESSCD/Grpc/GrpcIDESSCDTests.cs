095796e05d60c943d8af5c249858604f782e4b00
Paul Volavsek <paul@volavsek.com>
2025-03-27 15:20:17 +0100

    fix

--- test/fiskaltrust.Middleware.Interface.Client.Tests/IDESSCD/Grpc/GrpcIDESSCDTests.cs
+++ test/fiskaltrust.Middleware.Interface.Client.Tests/IDESSCD/Grpc/GrpcIDESSCDTests.cs
@@ -11,7 +11,7 @@ namespace fiskaltrust.Middleware.Interface.Client.Tests.IDESSCD.Grpc
     public class GrpcIDESSCDTests : IDESSCDTests
     {
         private string _host = "localhost";
-        private int _port = 40144;
+        private int _port = 40145;
         private static Server _server;
 
         ~GrpcIDESSCDTests()
