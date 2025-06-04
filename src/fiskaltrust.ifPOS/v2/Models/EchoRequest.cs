75e33ff943b4290463159946b845c08eba75a98b
PawelKarczewski <karczewskipawel.j@gmail.com>
2025-06-04 13:55:00 +0200

    Added v2 data model classes for Echo and Journal requests/responses

---
+++ src/fiskaltrust.ifPOS/v2/Models/EchoRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+#if NETSTANDARD2_1
+using System.Text.Json.Serialization;
+#endif
+
+namespace fiskaltrust.ifPOS.v2
+{
+    /// <summary>
+    /// Request to check if the communication to the Middleware is up and running. Body Contains a Message e.g. "Hello World!"
+    /// </summary>
+    public class EchoRequest
+    {
+#if NETSTANDARD2_1
+        [JsonPropertyName("Message")]
+#endif
+        [DataMember(Order = 1, EmitDefaultValue = true, IsRequired = true)]
+        public string Message { get; set; }
+    }
+}
