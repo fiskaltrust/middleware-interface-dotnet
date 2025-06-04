533ad3510c25577e6280571a1198e81735371fe0
PawelKarczewski <karczewskipawel.j@gmail.com>
2025-06-04 14:38:41 +0200

    Added v2 case flag enums for PayItem, Receipt, Signature, and State

---
+++ src/fiskaltrust.ifPOS/v2/Cases/PayItemCaseFlags.cs
@@ -0,0 +1,15 @@
+﻿namespace fiskaltrust.ifPOS.v2.Cases;
+
+public enum PayItemCaseFlags : long
+{
+    Void = 0x0001_0000,
+    Refund = 0x0002_0000,
+    Reserved = 0x0004_0000,
+    Downpayment = 0x0008_0000,
+    ForeignCurrency = 0x0010_0000,
+    Change = 0x0020_0000,
+    Tip = 0x0040_0000,
+    Electronic = 0x0080_0000,
+    Interface = 0x0100_0000,
+    ShowInChargeItems = 0x8000_0000,
+}
---
+++ src/fiskaltrust.ifPOS/v2/Cases/StateFlags.cs
@@ -0,0 +1,10 @@
+﻿namespace fiskaltrust.ifPOS.v2.Cases;
+
+public enum StateFlags : long
+{
+    SecurityMechanismDeactivated = 0x0000_0000_0000_0001,
+    SCUTemporaryOutOfService = 0x0000_0000_0000_0002,
+    LateSigningModeIsActive = 0x0000_0000_0000_0008,
+    MessageIsPending = 0x0000_0000_0000_0040,
+    DailyClosingIsDue = 0x0000_0000_0000_0100,
+}
