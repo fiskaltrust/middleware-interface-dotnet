57b486ad8822b6eaa15dac30495678b63dc7af5e
PawelKarczewski <karczewskipawel.j@gmail.com>
2025-06-04 16:53:29 +0200

    Added v2 case extensions and enums for ChargeItem, PayItem, Receipt, and Signature

--- src/fiskaltrust.ifPOS/v2/Cases/SignatureTypeFlags.cs
+++ src/fiskaltrust.ifPOS/v2/Cases/SignatureTypeFlags.cs
@@ -9,3 +9,9 @@ public enum SignatureTypeFlags
     DontVisualize = 0x0020_0000,
     DigitalReceiptOnly = 0x0030_0000,
 }
+
+public static class SignatureTypeFlagsExt
+{
+    public static SignatureType WithFlag(this SignatureType self, SignatureTypeFlags flag) => (SignatureType) ((long) self | (long) flag);
+    public static bool IsFlag(this SignatureType self, SignatureTypeFlags flag) => ((long) self & (long) flag) == (long) flag;
+}
