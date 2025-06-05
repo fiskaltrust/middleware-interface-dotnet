92e67c2fa8c1aeeb19ffdbd2a43b884a7b3494bf
PawelKarczewski <karczewskipawel.j@gmail.com>
2025-06-05 12:32:18 +0200

    Added extensions and utility methods for SignatureFormat enum

--- src/fiskaltrust.ifPOS/v2/Cases/SignatureFormat.cs
+++ src/fiskaltrust.ifPOS/v2/Cases/SignatureFormat.cs
@@ -1,4 +1,6 @@
-﻿namespace fiskaltrust.ifPOS.v2.Cases;
+﻿using System;
+
+namespace fiskaltrust.ifPOS.v2.Cases;
 public enum SignatureFormat : long
 {
     Unknown = 0x0000,
@@ -16,4 +18,18 @@ public enum SignatureFormat : long
     UPCA = 0x000B,
     Code39 = 0x000C,
     Base64 = 0x000D
+}
+
+public static class SignatureFormatExt
+{
+    public static SignatureFormat AsSignatureFormat(this long self) => (SignatureFormat) self;
+    public static T As<T>(this SignatureFormat self) where T : Enum, IConvertible => (T) Enum.ToObject(typeof(T), self);
+    public static SignatureFormat Reset(this SignatureFormat self) => (SignatureFormat) (0xFFFF_F000_0000_0000 & (ulong) self);
+
+    public static SignatureFormat WithVersion(this SignatureFormat self, byte version) => (SignatureFormat) ((((ulong) self) & 0xFFFF_0FFF_FFFF_FFFF) | ((ulong) version << (4 * 11)));
+    public static byte Version(this SignatureFormat self) => (byte) ((((long) self) >> (4 * 11)) & 0xF);
+
+    public static bool IsFormat(this SignatureFormat self, SignatureFormat format) => ((long) self & 0xFFFF) == (long) format;
+    public static SignatureFormat WithFormat(this SignatureFormat self, SignatureFormat state) => (SignatureFormat) (((ulong) self & 0xFFFF_FFFF_FFFF_0000) | (ulong) state);
+    public static SignatureFormat Format(this SignatureFormat self) => (SignatureFormat) ((long) self & 0xFFFF);
 }
