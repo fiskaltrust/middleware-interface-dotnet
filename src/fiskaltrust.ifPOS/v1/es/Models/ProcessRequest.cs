76a5bb456d091bba2ca1a0de9c7bd463446d9ed1
Leila <67684543+LeilaZarban@users.noreply.github.com>
2025-06-10 15:46:29 +0200

    Add-IESSSCD and models

---
+++ src/fiskaltrust.ifPOS/v1/es/Models/ProcessRequest.cs
@@ -0,0 +1,21 @@
+using System.Runtime.Serialization;
+
+namespace fiskaltrust.ifPOS.v1.es
+
+{
+    [DataContract]
+    public class ProcessRequest
+    {
+        [DataMember(Order =10)]
+        public required ReceiptRequest ReceiptRequest { get; set; }
+
+        [DataMember(Order = 20)]
+        public required ReceiptResponse ReceiptResponse { get; set; }
+
+        [DataMember(Order = 30)]
+        public required ReceiptRequest? PreviousReceiptRequest { get; set; }
+
+        [DataMember(Order = 40)]
+        public required ReceiptResponse? PreviousReceiptResponse { get; set; }
+    }
+}
