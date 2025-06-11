0dc46107fe4eb2550e1897eb0d282b01485e4064
Leila <67684543+LeilaZarban@users.noreply.github.com>
2025-06-11 15:06:52 +0200

    Use-IESSSCD-In-Clients

---
+++ src/fiskaltrust.Middleware.Interface.Client.Http/ESSSCD/HttpESSSCDFactory.cs
@@ -0,0 +1,27 @@
+﻿using fiskaltrust.ifPOS.v1.es;
+using fiskaltrust.Middleware.Interface.Client.Common.RetryLogic;
+using System.Threading.Tasks;
+
+namespace fiskaltrust.Middleware.Interface.Client.Http
+{
+    /// <summary>
+    /// Create Http SSCD.
+    /// </summary>
+    public static class HttpESSSCDFactory
+    {
+        public static async Task<IESSSCD> CreateSSCDAsync(HttpESSSCDClientOptions options)
+        {
+            var connectionhandler = new HttpProxyConnectionHandler<IESSSCD>(new HttpESSSCD(options));
+
+            if (options.RetryPolicyOptions != null)
+            {
+                var retryPolicyHelper = new RetryPolicyHandler<IESSSCD>(options.RetryPolicyOptions, connectionhandler);
+                return new ESSSCDRetryProxyClient(retryPolicyHelper);
+            }
+            else
+            {
+                return await connectionhandler.GetProxyAsync();
+            }
+        }
+    }
+}
