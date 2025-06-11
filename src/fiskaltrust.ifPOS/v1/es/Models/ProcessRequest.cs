8753f4550f9352a9b9c3a88bf7f0377d92690f83
Leila <67684543+LeilaZarban@users.noreply.github.com>
2025-06-11 10:55:09 +0200

    remove required word

--- src/fiskaltrust.ifPOS/v1/es/Models/ProcessRequest.cs
+++ src/fiskaltrust.ifPOS/v1/es/Models/ProcessRequest.cs
@@ -7,15 +7,15 @@ namespace fiskaltrust.ifPOS.v1.es
     public class ProcessRequest
     {
         [DataMember(Order = 10)]
-        public required ReceiptRequest ReceiptRequest { get; set; }
+        public ReceiptRequest ReceiptRequest { get; set; }
 
         [DataMember(Order = 20)]
-        public required ReceiptResponse ReceiptResponse { get; set; }
+        public ReceiptResponse ReceiptResponse { get; set; }
 
         [DataMember(Order = 30)]
-        public required ReceiptRequest? PreviousReceiptRequest { get; set; }
+        public ReceiptRequest? PreviousReceiptRequest { get; set; }
 
         [DataMember(Order = 40)]
-        public required ReceiptResponse? PreviousReceiptResponse { get; set; }
+        public ReceiptResponse? PreviousReceiptResponse { get; set; }
     }
 }
