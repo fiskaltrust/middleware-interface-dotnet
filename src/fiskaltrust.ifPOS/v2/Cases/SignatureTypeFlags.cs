b62754affb72bbf2ddb2ee93eb813bb922f543d7
Paul Volavsek <paul@volavsek.com>
2025-06-17 13:57:24 +0200

    fix

--- src/fiskaltrust.ifPOS/v2/Cases/SignatureTypeFlags.cs
+++ src/fiskaltrust.ifPOS/v2/Cases/SignatureTypeFlags.cs
@@ -5,13 +5,14 @@ public enum SignatureTypeFlags
     /// <value><c>0x0000_0000_0001_0000</c></value>
     ArchivingRequired = 0x0000_0000_0001_0000,
 
-    // These three are actually not flags but a case since 0x3 = 0x1 | 0x2
     /// <value><c>0x0000_0000_0010_0000</c></value>
     VisualizationOptional = 0x0000_0000_0010_0000,
     /// <value><c>0x0000_0000_0020_0000</c></value>
     DontVisualize = 0x0000_0000_0020_0000,
-    /// <value><c>0x0000_0000_0030_0000</c></value>
-    DigitalReceiptOnly = 0x0000_0000_0030_0000,
+    /// <value><c>0x0000_0000_0040_0000</c></value>
+    PrintedReceiptOnly = 0x0000_0000_0040_0000,
+    /// <value><c>0x0000_0000_0080_0000</c></value>
+    DigitalReceiptOnly = 0x0000_0000_0080_0000,
 }
 
 public static class SignatureTypeFlagsExt
