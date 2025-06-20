329aac4a364549d68b6b5f5b1f4cf5fb26065a06
Paul Volavsek <paul@volavsek.com>
2025-06-20 12:30:38 +0200

    readd v2 echo and journal

---
+++ src/fiskaltrust.ifPOS/v2/Models/JournalResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+#if NETCOREAPP3_0_OR_GREATER
+using System.Text.Json.Serialization;
+#endif
+
+namespace fiskaltrust.ifPOS.v2
+{
+    /// <summary>
+    /// The fiskaltrust.Middleware returns the requested JournalType as a FileStream.
+    /// </summary>
+    public class JournalResponse
+    {
+#if NETCOREAPP3_0_OR_GREATER
+        [JsonPropertyName("Chunk")]
+#endif
+        [DataMember(Order = 1, EmitDefaultValue = true, IsRequired = true)]
+        public List<byte> Chunk { get; set; }
+    }
+}
