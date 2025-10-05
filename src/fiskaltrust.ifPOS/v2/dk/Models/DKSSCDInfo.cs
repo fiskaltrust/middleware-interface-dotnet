a4974d682fe659c097f3f6a5cdd0c3f94fcd174c
PawelKarczewski <karczewskipawel.j@gmail.com>
2025-10-05 22:35:26 +0200

    Add v2 interfaces and data classes for BE and DK

---
+++ src/fiskaltrust.ifPOS/v2/dk/Models/DKSSCDInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace fiskaltrust.ifPOS.v2.dk
+{
+    public class DKSSCDInfo
+    {
+        public Dictionary<string, object> ExtraData { get; set; } = new Dictionary<string, object>();
+    }
+}
